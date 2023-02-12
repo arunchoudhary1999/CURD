48c3b69f3960debd4847381145154efe3455d3dd
Arun <123rjk04@gmail.com>
2023-02-12 12:09:41 +0530

    adding initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React JS CRUD Opertations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+}
+
+export default App;
